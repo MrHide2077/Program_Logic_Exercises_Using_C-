49efa5d45aceb4ed69628571ead4f99ea12182cf
MrHide2077 <heil.hide2077@gmail.com>
2026-08-25 01:33:17 -0500

    First C++ code commit

---
+++ First_attempt_C++/First_read_this_damn_thing.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hello World!\n";
+	cout << "LMAO dude, now I'm hacker. Go f**k everyone!\n";
+	cout << "As you can see I'm new here. Just the beginning folks, now wait for C++ exercises soon!!.\n";
+	return 0;
+}
