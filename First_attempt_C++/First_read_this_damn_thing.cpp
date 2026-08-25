370db560bfa60237c27390e896a80011b675cd60
MrHide2077 <heil.hide2077@gmail.com>
2026-08-25 01:45:02 -0500

    I forgot somethig

--- First_attempt_C++/First_read_this_damn_thing.cpp
+++ First_attempt_C++/First_read_this_damn_thing.cpp
@@ -4,5 +4,6 @@ int main() {
 	cout << "Hello World!\n";
 	cout << "LMAO dude, now I'm hacker. Go f**k everyone!\n";
 	cout << "As you can see I'm new here. Just the beginning folks, now wait for C++ exercises soon!!.\n";
+	cout << "Just wait...\n";"
 	return 0;
 }
